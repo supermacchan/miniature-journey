84bd219735b5f77b598b04aadff9525fe4fca6e2
Olga Birkovska <aiba.aki9@gmail.com>
2023-03-10 18:33:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/np-test-task'>
+    <BrowserRouter basename='/miniature-journey'>
       <App />
     </BrowserRouter> 
   </React.StrictMode>
